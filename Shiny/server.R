a97d8920016a73fba785e6f28a7f5176b21e6b70
Nicholas Sundin <nsundin2@gmail.com>
2015-02-12 02:07:26 -0600

    Fixed json.

--- Shiny/server.R
+++ Shiny/server.R
@@ -5,7 +5,7 @@ library(RJSONIO)
 # Limit the query to 500 items to speed up shiny app (nbd for now)
 
 shinyServer(function(input, output) {
-  s <- getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from DIAMONDS where ROWNUM <= 1500"'), 
+  s <- getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from DIAMONDS where ROWNUM <= 15000"'), 
               httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:orcl', 
                            USER='C##cs370_nos98', 
                            PASS='orcl_nos98', 
@@ -17,10 +17,12 @@ shinyServer(function(input, output) {
   
   sd <-fromJSON(s)
   sdf <<- do.call("rbind", sd)
+  data_frame <- sdf[-1,]
+  colnames(data_frame) <- sdf[1,]
 
   # a large table, reative to input$show_vars
   output$mytable1 <- renderDataTable({
     library(ggplot2)
-    sdf[, ]
+    data_frame[, ]
   })  
 })
