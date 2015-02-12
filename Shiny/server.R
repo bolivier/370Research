9eb04bfa80d9fe868a8076a073917144c0c0eabc
Nicholas Sundin <nsundin2@gmail.com>
2015-02-12 01:32:02 -0600

    Moved to json format.

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,20 +1,23 @@
 library(RCurl)
 library(shiny)
+library(RJSONIO)
 
 # Limit the query to 500 items to speed up shiny app (nbd for now)
 
 shinyServer(function(input, output) {
-  s <- getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select *  from DIAMONDS where ROWNUM <= 500"'), 
+  s <- getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from DIAMONDS where ROWNUM <= 1500"'), 
               httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:orcl', 
                            USER='C##cs370_nos98', 
                            PASS='orcl_nos98', 
                            MODE='native_mode', 
-                           MODEL='model', 
-                           returnFor = 'R'), 
+                           MODEL='model',
+                           returnDimensions = 'False',
+                           returnFor = 'JSON'), 
               verbose = TRUE) 
   
-  sdf <<- data.frame(eval(parse(text=substring(s, 1, 2^31-1))))
-  
+  sd <-fromJSON(s)
+  sdf <<- do.call("rbind", sd)
+
   # a large table, reative to input$show_vars
   output$mytable1 <- renderDataTable({
     library(ggplot2)
